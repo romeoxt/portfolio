e9e4c3164c27c86743ce00b33c7fb7ac35ea31e5
Fitzherbert LeGall <fitzherbert.legall@silkworldwide.com>
2024-04-22 15:51:31 -0500

    update vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   base: './', // This should be set correctly to reference assets relative to the index.html.
   build: {
     outDir: 'dist',  // Ensure this is correct
