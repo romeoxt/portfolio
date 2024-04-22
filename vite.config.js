a218bd05ec19f9b6083aa84c835cb48db347148b
Fitzherbert LeGall <fitzherbert.legall@silkworldwide.com>
2024-04-22 16:42:53 -0500

    Vite Updatte

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This should be set correctly to reference assets relative to the index.html.
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : './',
   build: {
     outDir: 'dist',  // Ensure this is correct
   }
