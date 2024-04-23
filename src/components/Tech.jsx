96ddca568a38a6e53dbfba5dc144aee7bcf702bc
romeoxt <herbylegall9@gmail.com>
2024-04-23 07:16:34 -0500

    Updates

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,7 +6,7 @@ const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className="w-28 h-28 key={technology.name}">
+        <div key={technology.name} className="w-28 h-28">
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Add this line to set the base option
+  base: '/portfolio/',
 });
