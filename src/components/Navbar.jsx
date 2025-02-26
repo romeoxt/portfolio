6231a4eb0b838c4885d66416db2322b003532ee0
romeoxt <109571033+romeoxt@users.noreply.github.com>
2025-02-26 11:20:14 -0600

    Update Navbar.jsx
    Add Nav component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Herby &nbsp;
-            <span className="sm:block hidden">| Fitzherbert LeGall</span>
+            <span className="sm:block hidden">| Fitzherbert LeGall IV</span>
             </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
