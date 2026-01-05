7852a008bcb1d081bbb11245e4bd454990b05f2b
romeoxt <herbylegall9@gmail.com>
2026-01-05 15:17:14 -0600

    Switch navbar to text brand

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { styles } from '../style';
 import { navLinks, socialLinks } from '../constants';
-import { logo, menu, close } from '../assets';
+import { menu, close } from '../assets';
 
 const Navbar = () => {
   const [active, setActive] = useState('')
@@ -22,11 +22,10 @@ const Navbar = () => {
           window.scrollTo(0, 0);
         }}
         >
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Herby &nbsp;
-            <span className="sm:block hidden">| Fitzherbert LeGall IV</span>
-            </p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex items-center gap-2">
+            <span>Fitzherbert</span>
+            <span className="text-secondary font-semibold">| Developer</span>
+          </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10 items-center'>
           {navLinks.map((nav) => (
